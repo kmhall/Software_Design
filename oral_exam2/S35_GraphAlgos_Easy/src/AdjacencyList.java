1cc149670e4f428c10ba270e58dd01a30df51878
K2mc484 <kylehall484@gmail.com>
2018-11-17 14:33:29 -0600

    java docs

--- oral_exam2/S35_GraphAlgos_Easy/src/AdjacencyList.java
+++ oral_exam2/S35_GraphAlgos_Easy/src/AdjacencyList.java
@@ -2,8 +2,16 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
 
+/**
+ * AdjacencyList is a class that takes a list of 5 letter words and creates a web connecting words with an edit distance of one.
+ * It prints the average number of connections, number of nodes with no edges, and the nodes with the most edges.
+ */
 public class AdjacencyList {
 
+    /**
+     * Main method for AdjacencyList
+     * @param args System inputs
+     */
     public static void main(String args[]) {
 
         File file = new File("./oral_exam2/S35_GraphAlgos_Easy/words.dat");
@@ -20,7 +28,12 @@ public class AdjacencyList {
 
     }
 
-    public static List<String> fileToListOfWords(File file){
+    /**
+     * fileToListOfWords converts a csv file to a list
+     * @param file CSV file
+     * @return List of words
+     */
+    private static List<String> fileToListOfWords(File file){
 
         List<String> words = new LinkedList<>();
 
@@ -33,8 +46,6 @@ public class AdjacencyList {
         }
         inputStream.useDelimiter("\n");
 
-
-
         //Iterate through file and add to a list of words
         if (inputStream.hasNext()) {
             while (inputStream.hasNext()) {
@@ -46,7 +57,12 @@ public class AdjacencyList {
         return words;
     }
 
-    public static  HashMap<String,List<String>> listOfWordsToHashMap( List<String> words){
+    /**
+     * Converts a list of words to a hash map
+     * @param words List of words
+     * @return HashMap with each word as a key and a list of words as the value.
+     */
+    private static  HashMap<String,List<String>> listOfWordsToHashMap( List<String> words){
 
         HashMap<String,List<String>> h1 = new HashMap<>();
 
@@ -64,6 +80,12 @@ public class AdjacencyList {
 
     }
 
+    /**
+     * Makes a list of words with an edit distance of one.
+     * @param currentWord String: word to find adjacent words
+     * @param words List: full list of words
+     * @return List of adjacent words
+     */
     private static List<String> arrayOfEditDistanceOfOne(String currentWord,List<String> words ){
 
         List<String> oneDistance = new LinkedList<>();
@@ -87,6 +109,12 @@ public class AdjacencyList {
         return oneDistance;
     }
 
+    /**
+     * Calculate the average number of connections, number of nodes with no edges, and the nodes with the most edges.
+     * @param words List of words
+     * @param h1 Hashmap of all keys with values
+     * @return String[] of all data
+     */
     private static String[] calculateData(List<String> words ,HashMap<String,List<String>> h1){
 
         String[] outputData = new String[4];
--- oral_exam2/S36_EditDistance_Hard/src/LevenshteinDistance.java
+++ oral_exam2/S36_EditDistance_Hard/src/LevenshteinDistance.java
@@ -1,3 +1,7 @@
+/**
+ * LevenshteinDistance is a class that calculates the edit distance between two words using the Levenshtein algorithm.
+ * https://en.wikipedia.org/wiki/Levenshtein_distance
+ */
 public class LevenshteinDistance {
 
     private String firstWord;
@@ -11,10 +15,10 @@ public class LevenshteinDistance {
     private int minimumDistance;
 
 
-
-
-
-
+    /**
+     * Main method for LevensheteinDistance
+     * @param args System inputs
+     */
     public static void main(String[] args){
 
         LevenshteinDistance distance = new LevenshteinDistance("abcdef","azced");
@@ -24,6 +28,12 @@ public class LevenshteinDistance {
     }
 
 
+    /**
+     * Constructs a LevenshteinDistance. Initilizes matrix with numbers counting
+     * from 0-length for first column and first row.
+     * @param firstWord String word to convert from
+     * @param secondWord String word to convert to
+     */
     public LevenshteinDistance(String firstWord, String secondWord){
 
         this.firstWord = firstWord;
@@ -44,6 +54,11 @@ public class LevenshteinDistance {
 
     }
 
+    /**
+     * Fills the matrix by by checking if the values in the [i-1] and [j-1] position are the same.
+     * If they are the same then convert the current to that. Otherwise find the min and convert current spot to
+     * the min +1 of the three
+     */
     public void fillMatrix(){
         for(int i=1;i<secondWord.length()+1;i++){
             for(int j=1;j<firstWord.length()+1;j++){
@@ -58,6 +73,12 @@ public class LevenshteinDistance {
         minimumDistance = matrix[secondWord.length()][firstWord.length()];
     }
 
+    /**
+     * Calculates the min of the left, up, and top left positions.
+     * @param i int: Current i position
+     * @param j int: Current j position
+     * @return int: min of three positions
+     */
     private int min(int i,int j){
         int min = matrix[i-1][j];
         if(matrix[i-1][j-1] < min){
@@ -70,7 +91,10 @@ public class LevenshteinDistance {
     }
 
 
-
+    /**
+     * Tracks back through all the position changes to print out what was changed. Each direction change corresponds to a
+     * replace/add/remove.
+     */
     public void backTrack(){
 
         int i = secondWord.length();
@@ -103,6 +127,12 @@ public class LevenshteinDistance {
         }
     }
 
+    /**
+     * Calcualates what position the current value was calculated from
+     * @param i int: Current i position
+     * @param j int Current j position
+     * @return String: corresponds to what position
+     */
     public String backTrackMin(int i, int j){
         int min = min(i,j);
         if(matrix[i-1][j-1] == min){
@@ -116,6 +146,10 @@ public class LevenshteinDistance {
         }
     }
 
+    /**
+     * Converts the matrix and words to a map to easy see the process in the terminal
+     * @return String of matrix with words
+     */
     @Override
     public String toString() {
 
